2a27200979b94144ee4d01e1527e4a11235d5dea
WyattMayor <wmayor@monmouthcollege.edu>
2022-01-25 09:30:01 -0600

    Lead and Gold Detection inprovement

--- src/Player1/MinerStrat.java
+++ src/Player1/MinerStrat.java
@@ -1,2 +1,54 @@
-package Player1;public class MinerStrat {
+package Player1;
+
+import battlecode.common.*;
+import java.util.Random;
+
+strictfp class MinerStrat {
+    /**
+     * Run a single turn for a Miner.
+     * This code is wrapped inside the infinite loop in run(), so it is called once per turn.
+     */
+    static void runMiner(RobotController rc) throws GameActionException {
+        // Try to mine on squares around us.
+        MapLocation me = rc.getLocation();
+        for (int dx = -1; dx <= 1; dx++) {
+            for (int dy = -1; dy <= 1; dy++) {
+                MapLocation mineLocation = new MapLocation(me.x + dx, me.y + dy);
+                // Notice that the Miner's action cooldown is very low.
+                // You can mine multiple times per turn!
+                while (rc.canMineGold(mineLocation)) {
+                    rc.mineGold(mineLocation);
+                }
+                while (rc.canMineLead(mineLocation)) {
+                    rc.mineLead(mineLocation);
+                }
+            }
+        }
+        int visionRadius = rc.getType().visionRadiusSquared;
+        MapLocation[] nearbyLocations = rc.getAllLocationsWithinRadiusSquared(me, visionRadius);
+
+        MapLocation targetLocation = null;
+        int distanceToTarget = Integer.MAX_VALUE;
+        for (MapLocation tryLocation : nearbyLocations) {
+            if (rc.senseLead(tryLocation) > 0 || rc.senseGold(tryLocation) > 0) {
+                int distanceTo = me.distanceSquaredTo(tryLocation);
+                if (distanceTo < distanceToTarget) {
+                    targetLocation = tryLocation;
+                    distanceToTarget = distanceTo;
+                }
+            }
+        }
+        if (targetLocation != null) {
+            Direction toMove = me.directionTo(targetLocation);
+            if (rc.canMove(toMove)) {
+                rc.move(toMove);
+            }
+        }
+        // Also try to move randomly.
+        Direction dir = RobotPlayer.directions[RobotPlayer.rng.nextInt(RobotPlayer.directions.length)];
+        if (rc.canMove(dir)) {
+            rc.move(dir);
+            System.out.println("I moved!");
+        }
+    }
 }

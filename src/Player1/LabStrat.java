1373b4dd2e94d4702d3a74c8856fb2f54488c31f
Braden Bernas <bradenbernas@hotmail.com>
2022-02-03 11:50:19 -0600

    laboratory changes

--- src/Player1/LabStrat.java
+++ src/Player1/LabStrat.java
@@ -4,7 +4,7 @@ import battlecode.common.*;
 
 public class LabStrat {
     public static void runLaboratory(RobotController rc) throws GameActionException {
-        if (rc.getTeamLeadAmount(rc.getTeam()) > 50 && rc.canTransmute()){
+        if (rc.getTeamLeadAmount(rc.getTeam()) > 200 && rc.canTransmute() && rc.getTeamGoldAmount(rc.getTeam()) < 100){
             rc.transmute();
         }
     }

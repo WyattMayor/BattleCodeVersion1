a54fda2a7aed47ad34b0ed3ac53185a4941da0db
Braden Bernas <bradenbernas@hotmail.com>
2022-02-03 11:55:58 -0600

    lab changes pt 2

--- src/Player1/LabStrat.java
+++ src/Player1/LabStrat.java
@@ -3,8 +3,12 @@ package Player1;
 import battlecode.common.*;
 
 public class LabStrat {
+    static int turns = 0;
     public static void runLaboratory(RobotController rc) throws GameActionException {
         if (rc.getTeamLeadAmount(rc.getTeam()) > 200 && rc.canTransmute() && rc.getTeamGoldAmount(rc.getTeam()) < 100){
+            rc.transmute();
+            turns++;
+        } else if(turns >= 1000){
             rc.transmute();
         }
     }
